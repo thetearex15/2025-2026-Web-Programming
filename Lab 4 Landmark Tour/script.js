4533c002f2e8124837fed2327f448d334355a4c1
Brian Vo <vobrian52@gmail.com>
2025-09-30 21:39:31 -0400

    radio buttons + image changing

---
+++ Lab 4 Landmark Tour/script.js
@@ -0,0 +1,12 @@
+function changePicture(value) {
+    var image = document.getElementById("changing_landmark");
+    if (value === 'The Roman Colosseum') {
+        image.src = "E:\\2025-2026-Web-Programming\\Lab 4 Landmark Tour\\Pictures\\Colosseum-Rome-Italy.png";
+    }
+    else if (value === 'The Great Sphinx of Giza') {
+        image.src = "E:\\2025-2026-Web-Programming\\Lab 4 Landmark Tour\\Pictures\\Great-Sphinx-dynasty-Giza.png";
+    }
+    else if (value === 'Moai') {
+        image.src = "E:\\2025-2026-Web-Programming\\Lab 4 Landmark Tour\\Pictures\\p0lmr1gk.jpg";
+    }
+}
