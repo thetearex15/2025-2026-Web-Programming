c801df453623837e6288d53f5090ed934b535a29
Brian Vo <vobrian52@gmail.com>
2025-12-01 17:57:12 -0500

    start Final Project

---
+++ Final Project/script.js
@@ -0,0 +1,5 @@
+quotes = [
+    "No one knows what he can do till he tries.",
+    "One must be poor to know the luxury of giving.",
+    "The average man, who does not know what to do with his life, wants another one which will last forever."
+]
