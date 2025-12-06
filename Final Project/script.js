9c97e8054a8d4ce9489986e25783a4daa8eb6e3b
Brian Vo <vobrian52@gmail.com>
2025-12-06 17:23:38 -0500

    Various bug fixes, and quotes are now translated into numbers

--- Final Project/script.js
+++ Final Project/script.js
@@ -5,18 +5,29 @@ function giveQuote() {
     "The average man, who does not know what to do with his life, wants another one which will last forever."
     ];
     var currQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    const origLetterToNumber = {
-        'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5,
-        'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
-        'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15,
-        'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20,
-        'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25,
-        'Z': 26
-        };
-    const changeToArray = Object.entries(origLetterToNumber);
-    const shuffledArray = changeToArray.sort(() => Math.random() - 0.5);
-    const shuffledLetterToNumber = Object.fromEntries(shuffledArray);
-    //change, this is just a test
+    const origLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const shuffledArray = origLetter.sort(() => Math.random() - 0.5);
+    var newQuote = "";
+    for (let i = 0; i < currQuote.length; i++) {
+        let foundChar = false;
+        if (currQuote.charAt(i) == " ") {
+            newQuote += "_";
+            continue;
+        }
+        for (let j = 0; j < shuffledArray.length; j++) {
+            if (currQuote.toUpperCase().charAt(i) == shuffledArray[j]) {
+                newQuote += j+1;
+                newQuote += " ";
+                foundChar = true;
+                break;
+            }
+        }
+        if (foundChar == false) {
+            newQuote += currQuote.charAt(i);
+        }
+    }
     var quoteDisplay = document.getElementById("quoteDisplay");
-    quoteDisplay.textContent = currQuote;
+    quoteDisplay.textContent = newQuote;
+    console.log(newQuote);
 }
+
