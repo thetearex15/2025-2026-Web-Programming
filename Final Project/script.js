6122b92e476e575f9dc44560231f654b36dadb16
Brian Vo <vobrian52@gmail.com>
2025-12-06 21:44:12 -0500

    added text boxes

--- Final Project/script.js
+++ Final Project/script.js
@@ -8,16 +8,27 @@ function giveQuote() {
     const origLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     const shuffledArray = origLetter.sort(() => Math.random() - 0.5);
     var newQuote = "";
+    var parentElement = document.getElementById("textBoxes");
+    //this doesnt work but i'd like it to reset the page when you click new game again
+        //let playedOnce = 0;
+        //if (playedOnce > 0)
+            //parentElement.remove(parentElement.children);
+    //converts letter to the random number, all letters are the same number
     for (let i = 0; i < currQuote.length; i++) {
         let foundChar = false;
         if (currQuote.charAt(i) == " ") {
-            newQuote += "_";
+            newQuote += " _ ";
             continue;
         }
         for (let j = 0; j < shuffledArray.length; j++) {
             if (currQuote.toUpperCase().charAt(i) == shuffledArray[j]) {
                 newQuote += j+1;
                 newQuote += " ";
+                var newTextBoxes = document.createElement("input");
+                newTextBoxes.setAttribute("maxlength", "1");
+                newTextBoxes.setAttribute("size", "1");
+                newTextBoxes.setAttribute("id", j.toString());
+                parentElement.appendChild(newTextBoxes);
                 foundChar = true;
                 break;
             }
@@ -28,6 +39,7 @@ function giveQuote() {
     }
     var quoteDisplay = document.getElementById("quoteDisplay");
     quoteDisplay.textContent = newQuote;
+    //playeOnce += 1;
     console.log(newQuote);
 }
 
